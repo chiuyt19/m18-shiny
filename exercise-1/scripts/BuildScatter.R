c965b4e51356ec5d217d5e481af5fa6c1c25ed7a
Cindy <chiuyt19@uw.edu>
2017-05-11 14:02:02 -0700

    add build function

--- exercise-1/scripts/BuildScatter.R
+++ exercise-1/scripts/BuildScatter.R
@@ -8,3 +8,9 @@
 #   - Title of the plot (set a default of "Title")
 #   - Label for the x axis (set a default of "X Title")
 #   - Label for the y axis (set a default of "Y Title")
+library(ggplot2)
+BuildScatter<-function(data,my_x,my_y,my_color){
+  p<-ggplot(data)+geom_point(mapping=aes(x=my_x,y=my_y,color=my_color))+labs(title="my plot",x="x axis",y="y axis")
+  return(p)
+}
+

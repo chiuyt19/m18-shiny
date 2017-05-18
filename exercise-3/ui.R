1ae2ff2581d5d275bcde7c7158a2d5b61b622338
Cindy <chiuyt19@uw.edu>
2017-05-17 17:44:32 -0700

    add exercise

--- exercise-3/ui.R
+++ exercise-3/ui.R
@@ -2,7 +2,7 @@
 shinyUI(fluidPage(
   mainPanel(
     # Add a selectInput (with a proper id) that allows you to select a variable to map
-    
+    selectInput('map',label='Variable of the Map', choices = list('Poplulation'='population','Votes'='votes','Vote/Population'='ratio')),
     # Use plotlyOutput to show your map
     plotlyOutput('map')
   )
